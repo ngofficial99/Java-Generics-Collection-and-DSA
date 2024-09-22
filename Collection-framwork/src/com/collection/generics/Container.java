05d711f76fb7ca9a56c41de83b9081604b24ec65
Nishant Gupta <ngofficial99@gmail.com>
2024-09-22 14:37:22 +0530

    implemented a generic interface in a non generic container

---
+++ Collection-framwork/src/com/collection/generics/Container.java
@@ -0,0 +1,13 @@
+package com.collection.generics;
+
+// Once we have an interface we can make this interface generic by adding the <T>
+public interface Container<T> {
+
+	// Now lets define a method in the Ingerface
+	
+	void add( T item);
+	
+	// Now I will create a class that is going to implement this interface
+	
+	T get();
+}
