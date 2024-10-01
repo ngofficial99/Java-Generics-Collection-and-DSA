6a5ae64a3b8fb8e60a813cdd146f6c6e9fa94551
Nishant Gupta <ngofficial99@gmail.com>
2024-10-01 14:07:57 +0530

    introduction to Lists in Java

---
+++ Collection-framwork/src/com/collection/list/main.java
@@ -0,0 +1,22 @@
+package com.collection.list;
+
+
+// What is a  List?
+// Lists are collection that maintain their elements in order and can contain duplicates
+// The elements in list are ordered. 
+// Lists are position based, starting from index 0.
+
+// List Methods:
+// E get(int index) : This is to get an element suing index.
+// E set(int index, E Element) : This is to modify the element, on an index.
+// void add(int index, E Element) : This is to add an new element on an index
+// E remove( int index) : this is to remove an element
+
+
+// Three implementations of List in Java are:
+// ArrayList
+// LinkedList
+// Vector
+public class main {
+
+}
